712294aa7da8941bbb47e3c69f7e01bedd090cd1
ShambaC <38806897+ShambaC@users.noreply.github.com>
2020-05-27 14:27:08 +0530

    First commit

---
+++ Assets/Scripts/Update_Check.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Update_Check : MonoBehaviour
+{
+    public string URL;
+    public Text update_text;
+
+    private int Current_Ver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Current_Ver = PlayerPrefs.GetInt("Version", 10);
+        StartCoroutine(GetRequest(URL));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GetRequest(string url)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if(webRequest.isNetworkError)
+            {
+                update_text.text = "Error :" + webRequest.error;
+            }
+            else
+            {
+                string Update_ver = webRequest.downloadHandler.text;                
+                if(int.Parse(Update_ver) > Current_Ver)
+                {                   
+                    update_text.text = "Update Required!";
+                }
+                else
+                {
+                    update_text.text = "You are up to date!";
+                }
+            }
+        }
+    }
+}

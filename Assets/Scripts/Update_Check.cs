c0f4467bd1c9ddf5228e7588edb69e550985cf3e
ShambaC <38806897+ShambaC@users.noreply.github.com>
2020-05-30 14:49:45 +0530

    Commit 1

--- Assets/Scripts/Update_Check.cs
+++ Assets/Scripts/Update_Check.cs
@@ -8,14 +8,21 @@ public class Update_Check : MonoBehaviour
 {
     public string URL;
     public Text update_text;
+    public int Set_Version;
+    public Text VersionTXT;
+    public GameObject Download_Button;
 
     private int Current_Ver;
+    private string PatchURL;
 
     // Start is called before the first frame update
     void Start()
     {
+        PlayerPrefs.SetInt("Version", Set_Version);
         Current_Ver = PlayerPrefs.GetInt("Version", 10);
         StartCoroutine(GetRequest(URL));
+        VersionTXT.text = "version: " + Current_Ver;
+        Download_Button.SetActive(false);
     }
 
     // Update is called once per frame
@@ -36,10 +43,14 @@ public class Update_Check : MonoBehaviour
             }
             else
             {
-                string Update_ver = webRequest.downloadHandler.text;                
+                string File_Data = webRequest.downloadHandler.text;
+                string[] TextArr = File_Data.Split('@');
+                string Update_ver = TextArr[0];              
                 if(int.Parse(Update_ver) > Current_Ver)
                 {                   
                     update_text.text = "Update Required!";
+                    Download_Button.SetActive(true);
+                    PatchURL = TextArr[1];
                 }
                 else
                 {
@@ -47,5 +58,10 @@ public class Update_Check : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void Download_Button_Method()
+    {
+        Application.OpenURL(PatchURL);
     }
 }
